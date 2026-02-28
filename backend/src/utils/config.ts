30ae79f5338ab8dc2aebc0f886a1b05111e4ed65
jonrhuang <jonrhuang@gmail.com>
2026-02-27 21:24:09 -0500

    changed env var names

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -1,17 +1,19 @@
 import OpenAI from 'openai';
 import { createClient } from "@supabase/supabase-js";
+import dotenv from 'dotenv';
+dotenv.config();
 
 /** OpenAI config */
-const openaiKey = process.env.VITE_OPENAI_API_KEY;
-if (!openaiKey) throw new Error("OpenAI API key is missing or invalid.");
+const openaiKey = process.env.OPENAI_API_KEY;
+if (!openaiKey) throw new Error('OpenAI API key is missing or invalid.');
 export const openai = new OpenAI({
   apiKey: openaiKey, 
   dangerouslyAllowBrowser: true
 });
 
 /** Supabase config */
-const supabaseKey = process.env.VITE_SUPABASE_API_KEY;
-if (!supabaseKey) throw new Error(`Expected env var SUPABASE_API_KEY`);
-const url = process.env.VITE_SUPABASE_URL;
-if (!url) throw new Error(`Expected env var SUPABASE_URL`);
+const supabaseKey = process.env.SUPABASE_API_KEY;
+if (!supabaseKey) throw new Error('Supabase API key is missing or invalid.');
+const url = process.env.SUPABASE_URL;
+if (!url) throw new Error('Supabase Url is missing or invalid');
 export const supabase = createClient(url, supabaseKey);
